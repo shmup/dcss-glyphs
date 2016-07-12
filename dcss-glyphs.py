6ee7c036d7e91339913b0cb2e1af3d9bb0528ed8
Jared Miller <jtmiller@gmail.com>
2016-07-11 23:59:26 -0400

    disabling autocomplete for mobile reasons

--- dcss-glyphs.py
+++ dcss-glyphs.py
@@ -140,7 +140,7 @@ html += """</p>
 <p>
 Mouseover a glyph to see its name. Click to see the accompany <a href="http://crawl.develz.org/info/index.php?q=butterfly" target="_blank">LearnDB</a> entry.
 </p>
-<p><input id="filter" type="text" placeholder="filter" autofocus></input> <button onclick=reset()>reset</button></p>
+<p><input id="filter" type="text" placeholder="filter" autocomplete="off" autofocus></input> <button onclick=reset()>reset</button></p>
 </div>
 <div id="glyphs">"""
 
